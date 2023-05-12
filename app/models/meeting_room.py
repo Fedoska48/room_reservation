05e1a6243aa72a21234029fe20d89ab7ad68ca2f
Никита Федяев <itsfedos@gmail.com>
2023-05-12 22:55:25 +0300

    init

---
+++ app/models/meeting_room.py
@@ -0,0 +1,17 @@
+# app/models/meeting_room.py
+
+# Импортируем из Алхимии нужные классы.
+from sqlalchemy import Column, String, Text
+
+# Импортируем базовый класс для моделей.
+from sqlalchemy.orm import relationship
+
+from app.core.db import Base
+
+
+class MeetingRoom(Base):
+    # Имя переговорки должно быть не больше 100 символов,
+    # уникальным и непустым.
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text)
+    reservations = relationship('Reservation', cascade='delete')
