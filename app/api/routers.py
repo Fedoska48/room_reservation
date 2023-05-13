a36c305dd609bc5cab1660915ff23caf7e235e13
Никита Федяев <itsfedos@gmail.com>
2023-05-13 17:26:45 +0300

    user model

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 # app/api/routers.py
 from fastapi import APIRouter
 
-from app.api.endpoints import meeting_room_router, reservation_router
+from app.api.endpoints import meeting_room_router, reservation_router, \
+    user_router
 
 main_router = APIRouter()
 main_router.include_router(
@@ -10,3 +11,5 @@ main_router.include_router(
 main_router.include_router(
     reservation_router, prefix='/reservations', tags=['Reservations']
 )
+
+main_router.include_router(user_router)
